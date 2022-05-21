96d3e6b558c2ea688677bc8758e4327e634d0ab7
atetheone <atevirran@gmail.com>
2022-05-21 20:49:31 +0100

    login quick setup

--- sudolink-ui/src/app/components/login/login.component.ts
+++ sudolink-ui/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  });
   constructor() { }
 
   ngOnInit(): void {
